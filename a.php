ea05ce948a4614b0a51180bc97648ad493df2af8
i013amo <139213418+i013amo@users.noreply.github.com>
2023-07-11 17:33:36 +0900

    Update a.php

--- a.php
+++ a.php
@@ -2,7 +2,7 @@
 <?php 
   foreach(file('/usr/share/dict/linux.words') as $line){
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-    if(strpos($line,"abc" !== false)){
+    if(strpos($line,"abc") !== false){
        echo "$line";
     }
   } 
