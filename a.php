ec4c0559c1a0d342b10297e890a23455309772d8
i013amo <139213418+i013amo@users.noreply.github.com>
2023-07-19 10:34:51 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,8 +1,8 @@
 #!/usr/bin/php
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
-    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-    if(strpos($line,"abc") !== false){
+    // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+    if(strpos($line,"xy") !== false){
        echo "$line";
     }
   } 
